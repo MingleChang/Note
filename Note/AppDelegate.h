c15b9826ca399f2f32158a3547bdf51aa5848279
Mingle Chang <mingle890805@gmail.com>
2015-05-01 22:51:52 +0800

    Initial Commit

---
+++ Note/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Note
+//
+//  Created by 常峻玮 on 15/5/1.
+//  Copyright (c) 2015年 Mingle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Note/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Note
+//
+//  Created by 常峻玮 on 15/5/1.
+//  Copyright (c) 2015年 Mingle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
