02a0dc45d08f57672dabc18d483fcae267c2dc53
Chang Junwei <mingle890805@gmail.com>
2015-05-05 17:33:00 +0800

    add text change

--- Note/Views/MCTextView.m
+++ Note/Views/MCTextView.m
@@ -15,6 +15,9 @@
 @end
 
 @implementation MCTextView
+-(void)dealloc{
+    [[NSNotificationCenter defaultCenter]removeObserver:self];
+}
 -(instancetype)initWithFrame:(CGRect)frame{
     self= [super initWithFrame:frame];
     if (self) {
@@ -48,7 +51,7 @@
     [self setupPlaceholderLabel];
 }
 -(void)initAllData{
-    
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(textChanged:) name:UITextViewTextDidChangeNotification object:nil];
 }
 -(void)setupPlaceholderLabel{
     self.placeholderLabel=[[UILabel alloc]init];
@@ -62,10 +65,21 @@
     self.lableWidthConstraint=[NSLayoutConstraint constraintWithItem:self attribute:NSLayoutAttributeWidth relatedBy:NSLayoutRelationEqual toItem:self.placeholderLabel attribute:NSLayoutAttributeWidth multiplier:1.0 constant:10+self.textContainerInset.left+self.textContainerInset.right];
     [self addConstraints:@[self.lableLeftConstraint,self.lableTopConstraint,self.lableWidthConstraint]];
 }
+#pragma mark - Notifaction
+-(void)textChanged:(NSNotification *)sender{
+    if (sender.object!=self) {
+        return;
+    }
+    self.placeholderLabel.hidden=(self.text.length>0||self.placeholderString.length==0);
+}
 #pragma mark - Setter And Getter
 -(void)setFont:(UIFont *)font{
     [super setFont:font];
     self.placeholderLabel.font=font;
+}
+-(void)setText:(NSString *)text{
+    [super setText:text];
+    self.placeholderLabel.hidden=(text.length>0||self.placeholderString.length==0);
 }
 -(UIColor *)placeholderColor{
     return self.placeholderLabel.textColor;
