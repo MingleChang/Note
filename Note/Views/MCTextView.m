af22fdee13fd20baeb7148042cbc1ea9a655cc93
Chang Junwei <mingle890805@gmail.com>
2015-05-05 17:01:55 +0800

    change

--- Note/Views/MCTextView.m
+++ Note/Views/MCTextView.m
@@ -9,6 +9,9 @@
 #import "MCTextView.h"
 @interface MCTextView()
 @property(nonatomic,strong)UILabel *placeholderLabel;
+@property(nonatomic,strong)NSLayoutConstraint *lableLeftConstraint;
+@property(nonatomic,strong)NSLayoutConstraint *lableTopConstraint;
+@property(nonatomic,strong)NSLayoutConstraint *lableWidthConstraint;
 @end
 
 @implementation MCTextView
@@ -52,13 +55,12 @@
     [self.placeholderLabel setTranslatesAutoresizingMaskIntoConstraints:NO];
     self.placeholderColor=[UIColor colorWithRed:190.0/255 green:188.0/255 blue:188.0/255 alpha:1.0];
     self.placeholderLabel.font=self.font;
-    self.placeholderLabel.text=@"asfasdfasdasdfasdfasasfaaall";
     [self addSubview:self.placeholderLabel];
     
-    NSLayoutConstraint *lLabelLeftConstraint=[NSLayoutConstraint constraintWithItem:self.placeholderLabel attribute:NSLayoutAttributeLeading relatedBy:NSLayoutRelationEqual toItem:self attribute:NSLayoutAttributeLeading multiplier:1.0 constant:5];
-    NSLayoutConstraint *lLabelTopConstraint=[NSLayoutConstraint constraintWithItem:self.placeholderLabel attribute:NSLayoutAttributeTop relatedBy:NSLayoutRelationEqual toItem:self attribute:NSLayoutAttributeTop multiplier:1.0 constant:8];
-    NSLayoutConstraint *lLabelRightConstraint=[NSLayoutConstraint constraintWithItem:self attribute:NSLayoutAttributeWidth relatedBy:NSLayoutRelationEqual toItem:self.placeholderLabel attribute:NSLayoutAttributeWidth multiplier:1.0 constant:10];
-    [self addConstraints:@[lLabelLeftConstraint,lLabelTopConstraint,lLabelRightConstraint]];
+    self.lableLeftConstraint=[NSLayoutConstraint constraintWithItem:self.placeholderLabel attribute:NSLayoutAttributeLeading relatedBy:NSLayoutRelationEqual toItem:self attribute:NSLayoutAttributeLeading multiplier:1.0 constant:5+self.textContainerInset.left];
+    self.lableTopConstraint=[NSLayoutConstraint constraintWithItem:self.placeholderLabel attribute:NSLayoutAttributeTop relatedBy:NSLayoutRelationEqual toItem:self attribute:NSLayoutAttributeTop multiplier:1.0 constant:self.textContainerInset.top];
+    self.lableWidthConstraint=[NSLayoutConstraint constraintWithItem:self attribute:NSLayoutAttributeWidth relatedBy:NSLayoutRelationEqual toItem:self.placeholderLabel attribute:NSLayoutAttributeWidth multiplier:1.0 constant:10+self.textContainerInset.left+self.textContainerInset.right];
+    [self addConstraints:@[self.lableLeftConstraint,self.lableTopConstraint,self.lableWidthConstraint]];
 }
 #pragma mark - Setter And Getter
 -(void)setFont:(UIFont *)font{
@@ -77,12 +79,22 @@
 -(void)setPlaceholderString:(NSString *)placeholderString{
     self.placeholderLabel.text=placeholderString;
 }
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect {
-    // Drawing code
+-(void)setTextContainerInset:(UIEdgeInsets)textContainerInset{
+    [super setTextContainerInset:textContainerInset];
+    self.lableLeftConstraint.constant=5+self.textContainerInset.left;
+    self.lableTopConstraint.constant=self.textContainerInset.top;
+    self.lableWidthConstraint.constant=10+self.textContainerInset.left+self.textContainerInset.right;
 }
-*/
+//-(void)setContentOffset:(CGPoint)contentOffset{
+//    contentOffset.y=0;
+//    [super setContentOffset:contentOffset];
+//}
+/*
+ // Only override drawRect: if you perform custom drawing.
+ // An empty implementation adversely affects performance during animation.
+ - (void)drawRect:(CGRect)rect {
+ // Drawing code
+ }
+ */
 
 @end
