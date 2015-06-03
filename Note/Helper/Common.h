b9d9379400b6423cc11f2b40469aa3f0ef871844
Chang Junwei <mingle890805@gmail.com>
2015-06-03 16:33:56 +0800

    change

--- Note/Helper/Common.h
+++ Note/Helper/Common.h
@@ -13,6 +13,13 @@
 #define ONE_PIXELS 1/[UIScreen mainScreen].scale
 #define RGB(r,g,b) [UIColor colorWithRed:r/255.0 green:g/255.0 blue:b/255.0 alpha:1.0]
 #define RGBA(r,g,b,a) [UIColor colorWithRed:r/255.0 green:g/255.0 blue:b/255.0 alpha:a]
+
+#ifdef DEBUG
+
+#else
+
+#endif
+
 @interface Common : NSObject
 
 @end
