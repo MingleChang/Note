abb755fc6e503d4812efa2a23033af7c363c8700
Chang Junwei <mingle890805@gmail.com>
2015-06-04 15:16:46 +0800

    change VC

---
+++ Note/Controllers/MCRootViewController.h
@@ -0,0 +1,13 @@
+//
+//  MCRootViewController.h
+//  Note
+//
+//  Created by 常峻玮 on 15/6/4.
+//  Copyright (c) 2015年 Mingle. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface MCRootViewController : ViewController
+
+@end
